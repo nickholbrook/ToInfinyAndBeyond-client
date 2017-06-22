375737c2b4e728d55e12e86e629e59262189c48e
nickholbrook <nick.holbrook@gmail.com>
2017-06-22 15:19:45 +0100

    Amended path to key

--- createInfinyBlog.sh
+++ createInfinyBlog.sh
@@ -6,6 +6,6 @@ if (( $# < 2))
 		echo "Type 'aws ec2 run-instances help' to see available Instance types"
 		exit 1
 	else
-		aws ec2 run-instances --image-id ami-d67c6ab2 --security-group-ids sg-db7a06b2 --count $1 --instance-type $2 --placement AvailabilityZone=eu-west-2a --user-data file://bootstrap.txt --key-name nick-keypair
+		aws ec2 run-instances --image-id ami-d67c6ab2 --security-group-ids sg-db7a06b2 --count $1 --instance-type $2 --placement AvailabilityZone=eu-west-2a --user-data file://bootstrap.txt --key-name /home/nick/nick-keypair
 fi
 
